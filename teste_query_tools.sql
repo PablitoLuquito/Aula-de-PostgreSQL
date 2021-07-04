a55f91bd0b2837f3369fcb221f9273151b34fc06
PablitoLuquito <pablolucas-1@outlook.com>
2021-07-04 12:14:41 -0300

    Primeiro Commit

---
+++ teste_query_tools.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS teste (
+	cpf VARCHAR(11) PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO teste (cpf,nome,created_at)
+VALUES ('12345678900','José Colméia','2020-07-04 12:00:00') ON CONFLICT (cpf) DO NOTHING;
+
+UPDATE teste SET nome = 'Pedro Alvares' WHERE cpf = '12345678900';
+
+SELECT * FROM teste;
